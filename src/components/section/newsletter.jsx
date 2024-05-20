61206623e670352cacb8168bc9aa51a4aa6cf1dc
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-05-20 14:36:53 +0100

    done with basic structure of home page

---
+++ src/components/section/newsletter.jsx
@@ -0,0 +1,28 @@
+import Button from "../button/button";
+import jumImage from '../../assets/store-graphics.svg'
+import backgroundImage from '../../assets/hero-custom.png'
+
+export default function Newsletter() {
+    return <div className="mt-5 " style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="w-[70vw] mx-auto flex p-8">
+            <div className='flex-1'>
+                <div className='font-bold text-3xl mb-3'>
+                    Get top deals, latest trends, and more.
+                </div>
+                <div className='font-normal text-lg mb-3'>
+                    Join our email subscription now to get updates on promotions and coupons.
+                </div>
+                <div className='flex gap-3'>
+                    <input placeholder='Email Address' type="text" className='border w-[30vw] rounded-lg h-[40px] px-4 focus:outline-none focus:border-2' />
+
+                    <Button text="Subscribe" color="text-white" bgColor="bg-red-500" bgHover="hover:bg-red-600" />
+
+                </div>
+            </div>
+            <div className='flex-1 hidden lg:block'>
+                <img className='' src={jumImage} alt="" />
+
+            </div>
+        </div>
+    </div>
+}
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,16 +2,36 @@
 import React from 'react';
 import Navbar from '../components/nav/navbar.jsx';
 import Hero from '../components/section/hero.jsx';
-import HomeCategory from '../components/section/homeCategory.jsx';
+import MidJumbotron from '../components/section/midJombotron.jsx';
+import PetsCategory from '../components/card/petsCategory.jsx';
+import PetCard from '../components/card/petsCard.jsx';
+import Footer from '../components/section/footer.jsx';
+import Newsletter from '../components/section/newsletter.jsx';
 
 const Home = () => {
     return (
         <>
             <Navbar />
             <Hero />
-            <div className='px-5 lg:px-24'>
-                <HomeCategory />
-            </div>        </>
+            <div className='px-5 lg:px-24 pb-7'>
+                <div className='font-semibold text-2xl my-5'>Pets Category</div>
+                <div className='overflow-x-auto flex pb-5'>
+                    <PetsCategory />
+                    <PetsCategory />
+                    <PetsCategory />
+                </div>
+                <MidJumbotron />
+                <div className='font-semibold text-2xl my-5'>Popular Pets</div>
+                <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <PetCard />
+                    <PetCard />
+                    <PetCard />
+                    <PetCard />
+                </div>
+            </div>
+            <Newsletter />
+            <Footer />
+        </>
     );
 };
 
