e991fe7141326b2c1eda3a50c959e260dcbff9f8
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-12-14 14:08:32 +0100

    worked on mobile navbar

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,5 +1,5 @@
-export default function Button({ text, bgColor, color, extraClass = "" }) {
-    return <button className={`py-2 px-3 rounded ${bgColor} ${color} ${extraClass}`}>
+export default function Button({ text, bgColor, color, bgHover, extraClass = "" }) {
+    return <button className={`py-2 px-3 rounded ${bgColor} ${bgHover} ${color} ${extraClass}`}>
         {text}
     </button>
 }
