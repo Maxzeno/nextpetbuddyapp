762e114a38a82dbf6774053338333e9fb8d2a58a
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-12-19 06:38:24 +0100

    working sidebar

--- src/components/section/sidebar.jsx
+++ src/components/section/sidebar.jsx
@@ -1,48 +1,81 @@
-import { Cart, List, XLg } from "react-bootstrap-icons";
-import Navbar from '../../components/nav/navbar'
 import { useState } from "react";
+import { Cart, MenuButton, XLg } from "react-bootstrap-icons";
+import Navbar from "../../components/nav/navbar";
 import useScreenWidth from "../../hooks/useScreenWidth";
 
 export default function Sidebar({ head, body }) {
-    const [openDropdown, setOpenDropdown] = useState(null);
-    const screenWidth = useScreenWidth();
+  const [openDropdown, setOpenDropdown] = useState(null);
+  const screenWidth = useScreenWidth();
 
-    const toggleDropdown = (dropdown) => {
-        setOpenDropdown(openDropdown === dropdown ? null : dropdown);
-    };
+  const toggleDropdown = (dropdown) => {
+    setOpenDropdown(openDropdown === dropdown ? null : dropdown);
+  };
 
-    return <>
-        <div className={`${openDropdown === 'mobile' && 'absolute w-screen h-screen top-0 left-0 z-50 bg-white'}`}>
-            {
-                openDropdown === 'mobile' && screenWidth < 1024 ? <div className="flex justify-between  w-[80vw] mx-auto pt-5">
-                    <div>Menu</div>
-                    <div><XLg className='text-[1.1em] leading-[0]' /></div>
-                </div> : <Navbar />
-            }
+  return (
+    <>
+      <div
+        className={`${
+          openDropdown === "mobile" &&
+          "absolute w-screen h-screen top-0 left-0 z-50 bg-white"
+        }`}
+      >
+        {openDropdown === "mobile" && screenWidth < 1024 ? (
+          <div className="flex justify-between lg:w-[80vw] w-[90vw] mx-auto pt-5">
+            <div>Menu</div>
+            <div>
+              <XLg
+                className="text-[1.1em] leading-[0]"
+                onClick={() => toggleDropdown("mobile")}
+              />
+            </div>
+          </div>
+        ) : (
+          <Navbar />
+        )}
 
-            <div className='mx-auto pt-5 w-[80vw] mb-5' >
-                <div className='lg:flex lg:shrink-0 lg:grow-0'>
-                    <div className={`mt-5 lg:mt-10 w-full lg:w-[25vw] ${screenWidth < 1024 ? openDropdown !== 'mobile' && 'hidden' : "block"}`}>
-                        <div className="pb-5 mb-5 border-b">
-                            <div className="flex gap-3 items-center bg-purple-950 text-white rounded-md py-2 px-4">
-                                <Cart className='text-[1.1em] leading-[0]' />
-                                <span>Cart</span>
-                            </div>
-                            <div className="flex gap-3 items-center text-black rounded-m py-2 px-4">
-                                <Cart className='text-[1.1em] leading-[0]' />
-                                <span>Cart</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="flex justify-between">
-                            <div>{head}</div>
-                            <List className='text-[1.7em] cursor-pointer leading-[0] inline text-black lg:hidden' onClick={() => toggleDropdown("mobile")} />
-                        </div>
-                        <div>{body}</div>
-                    </div>
+        <div className="mx-auto w-[90vw] lg:w-[80vw] pb-5">
+          <div className="lg:flex lg:shrink-0 lg:grow-0">
+            <div
+              className={`pt-5 lg:pt-10 w-full lg:w-[25vw] lg:border-r-2 ${
+                screenWidth < 1024
+                  ? openDropdown !== "mobile" && "hidden"
+                  : "block"
+              }`}
+            >
+              <div className="pb-5 mb-5 border-b-2 pr-auto lg:mr-10">
+                <div className="flex gap-3 items-center bg-gray-950 text-white rounded-md py-2 px-4">
+                  <Cart className="text-[1.1em] leading-[0]" />
+                  <span>Cart</span>
                 </div>
-            </div >
+                <div className="flex gap-3 items-center text-black rounded-m py-2 px-4">
+                  <Cart className="text-[1.1em] leading-[0]" />
+                  <span>Cart</span>
+                </div>
+              </div>
+              <div className="flex gap-3 items-center text-black rounded-m py-2 px-4">
+                <Cart className="text-[1.1em] leading-[0]" />
+                <span>Log out</span>
+              </div>
+            </div>
+
+            {openDropdown !== "mobile" && (
+              <div className="pl-auto lg:pl-10 pt-3">
+                <div className="flex justify-between">
+                  <div>{head}</div>
+
+                  <span
+                    className="cursor-pointer lg:hidden"
+                    onClick={() => toggleDropdown("mobile")}
+                  >
+                    <MenuButton className="text-[1.6em] leading-[0] inline bg-gray-200 hover:bg-gray-300 py-1 rounded" />
+                  </span>
+                </div>
+                <div>{body}</div>
+              </div>
+            )}
+          </div>
         </div>
+      </div>
     </>
+  );
 }
