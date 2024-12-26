520ece3cc5d8733d6c895cc2024cdac8a0bf4d8b
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-12-26 15:25:20 +0100

    worked on order

---
+++ src/helper/dateFormat.js
@@ -0,0 +1,8 @@
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+};
+
+export { formatDate };
+
--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/section/footer.jsx";
+import Loading from "../components/section/loading.jsx";
 import Sidebar from "../components/section/sidebar.jsx";
+import { formatDate } from "../helper/dateFormat.js";
+import useFetch from "../hooks/useFetch.js";
 
 export default function Orders() {
+  const [ordersData, ordersLoading, ordersError] = useFetch("/order/");
+
   return (
     <>
       <Sidebar
@@ -18,40 +23,41 @@ export default function Orders() {
                   <td className="py-2 px-4 border-t font-medium">Status</td>
                   <td className="py-2 px-4 border-t font-medium">Date</td>
                 </tr>
-                <tr>
-                  <td className="py-2 px-4 border-t">
-                    <Link to="#" className="hover:text-amber-600">
-                      3hjh33j
-                    </Link>
-                  </td>
-                  <td className="py-2 px-4 border-t">Yes</td>
-                  <td className="py-2 px-4 border-t">
-                    <span className="bg-gray-400 px-2 text-white rounded text-xs py-1 font-medium">
-                      Pending
-                    </span>
-                  </td>
-                  <td className="py-2 px-4 border-t whitespace-nowrap">
-                    August 22, 2023
-                  </td>
-                </tr>
-                <tr>
-                  <td className="py-2 px-4 border-t">
-                    <Link to="#" className="hover:text-amber-600">
-                      ejdjkee
-                    </Link>
-                  </td>
-                  <td className="py-2 px-4 border-t">Yes</td>
-                  <td className="py-2 px-4 border-t">
-                    <span className="bg-gray-400 px-2 text-white rounded text-xs py-1 font-medium">
-                      Pending
-                    </span>
-                  </td>
-                  <td className="py-2 px-4 border-t whitespace-nowrap">
-                    August 22, 2023
-                  </td>
-                </tr>
+                {!ordersLoading &&
+                  !ordersError &&
+                  ordersData.map((item, index) => (
+                    <tr key={index}>
+                      <td className="py-2 px-4 border-t">
+                        <Link
+                          to={`/order/${item.id}`}
+                          className="hover:text-amber-600"
+                        >
+                          {item.id}
+                        </Link>
+                      </td>
+                      <td className="py-2 px-4 border-t">
+                        {item.has_paid ? "Yes" : "No"}
+                      </td>
+                      <td className="py-2 px-4 border-t">
+                        <span className="bg-gray-400 px-2 text-white rounded text-xs py-1 font-medium">
+                          {item.status === "P"
+                            ? "Pending"
+                            : item.status === "S"
+                            ? "Success"
+                            : "Cancelled"}
+                        </span>
+                      </td>
+                      <td className="py-2 px-4 border-t whitespace-nowrap">
+                        {formatDate(item.created_at)}
+                      </td>
+                    </tr>
+                  ))}
               </tbody>
             </table>
+            {ordersLoading && (
+              <Loading extraClass="flex justify-center item-center pb-5" />
+            )}
+            {ordersError && <div>Failed to fetch</div>}
           </div>
         }
       />
