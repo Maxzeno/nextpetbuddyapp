9e356929ebd32254174aa1ded784f32ca6d3beba
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-12-28 23:40:16 +0100

    fixed bug on confirm email

--- src/pages/confirmEmail.jsx
+++ src/pages/confirmEmail.jsx
@@ -1,11 +1,15 @@
 import { Link, useLocation } from "react-router-dom";
 import Navbar from "../components/nav/navbar";
+import Loading from "../components/section/loading";
 import useFetch from "../hooks/useFetch";
 
 export default function ConfirmEmail() {
   let location = useLocation();
+  if (!location.pathname.endsWith("/")) {
+    location.pathname += "/";
+  }
   const [emailConfirmData, emailConfirmLoading, emailConfirmError] = useFetch(
-    `${location.pathname}/`,
+    `${location.pathname}`,
     false
   );
 
@@ -17,16 +21,29 @@ export default function ConfirmEmail() {
 
       <div className="flex items-center justify-center mt-10">
         <div className="bg-white p-8 rounded-lg shadow-md">
-          <h1 className="text-3xl font-semibold text-gray-800 mb-4">
-            Email Confirmation
-          </h1>
-          <span className="text-gray-600 mb-2">{emailConfirmData?.detail}</span>
+          {!emailConfirmLoading && (
+            <h1 className="text-3xl font-semibold text-gray-800 mb-4">
+              Email Confirmation
+            </h1>
+          )}
+          {!emailConfirmLoading && !emailConfirmError && (
+            <div>
+              <span className="text-gray-600 mb-2">
+                {emailConfirmData?.detail}
+              </span>
 
-          <span className="text-gray-600 ml-1">
-            <Link to="/login" className="text-blue-500 hover:text-blue-400">
-              login
-            </Link>
-          </span>
+              <span className="text-gray-600 ml-1">
+                <Link to="/login" className="text-blue-500 hover:text-blue-400">
+                  login
+                </Link>
+              </span>
+            </div>
+          )}
+          {emailConfirmError && <div>{emailConfirmError.detail}</div>}
+
+          {emailConfirmLoading && (
+            <Loading extraClass="flex justify-center item-center" />
+          )}
         </div>
       </div>
     </div>
