289d6f68c095f8b9dd7ef8496b0e77b47adf954e
Emmanuel Nwaegunwa <mmnlchidera@gmail.com>
2024-12-28 07:36:00 +0100

    worked on reset and forgot password

--- src/pages/resetPassword.jsx
+++ src/pages/resetPassword.jsx
@@ -1,11 +1,49 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import image from "../assets/signin-g.svg";
 import Button from "../components/button/button";
 import NavbarAuth from "../components/nav/navbarAuth.jsx";
 import Footer from "../components/section/footer.jsx";
+import submitHelper from "../helper/submit.js";
 
 export default function ResetPassword() {
+  let location = useLocation();
+  console.log(location.pathname);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const onSubmit = async (data) => {
+    setLoading(true);
+
+    try {
+      const [result, errorObj] = await submitHelper(
+        location.pathname,
+        data,
+        false,
+        "PUT"
+      );
+      console.log(errorObj, "errorObj");
+      if (!errorObj) {
+        toast.success("Password reset successful");
+        navigate("/login");
+      } else {
+        toast.error(errorObj.detail || "An error occurred");
+      }
+    } catch (error) {
+      toast.error("An error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <NavbarAuth
@@ -26,24 +64,50 @@ export default function ResetPassword() {
         <div className="order-1 lg:order-2">
           <div className="text-3xl font-medium">Reset your password</div>
           <div className="pb-5 pt-2">Please enter your new passwords</div>
-          <form action="post">
+          <form action="post" onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3 mt-2">
               <input
+                {...register("password", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters long",
+                  },
+                })}
                 placeholder="new password"
-                type="text"
+                type="password"
                 className="border w-full rounded-lg h-[40px] px-4 focus:outline-2 outline-gray-300"
               />
+              {errors.password && (
+                <p className="text-sm text-red-500 pt-1">
+                  {errors.password.message}
+                </p>
+              )}
             </div>
             <div className="mb-3 mt-2">
               <input
+                {...register("password_again", {
+                  required: "This field is required",
+                  minLength: {
+                    value: 8,
+                    message: "Password must be at least 8 characters long",
+                  },
+                })}
                 placeholder="again"
                 type="password"
                 className="border w-full rounded-lg h-[40px] px-4 focus:outline-2 outline-gray-300"
               />
+              {errors.password_again && (
+                <p className="text-sm text-red-500 pt-1">
+                  {errors.password_again.message}
+                </p>
+              )}
             </div>
 
             <Button
-              text="Reset password"
+              type="submit"
+              disabled={loading}
+              text={loading ? "Loading..." : "Reset password"}
               color="text-white"
               extraClass="block w-full rounded-lg my-4"
               bgColor="bg-amber-600"
